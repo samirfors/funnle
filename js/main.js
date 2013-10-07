c6ab5a581eca2665b1f8d6e206d2dacf0364a7ef
Adam Gardefjord <adamgardefjord@gmail.com>
2013-10-07 20:57:03 +0200

    Mobile fquery ixes

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
-
 $(document).ready(function(){
 	//set the initial body width
 	var windowH = window.innerHeight; 
 	/*I need to go through all target divs because i don't know 
 	how many divs are here and what are their initial height*/
-
  	function resize() {
 	    //get the initial height of every div
 	    var windowH = window.innerHeight; 
 	    var windowW = window.innerWidth; 
-	    var videoHeaderH = window.innerHeight - 50;
+	    var videoHeaderH = window.outerHeight - 50;
 	    //sets the different height for every needed div
 	    $(".video_container").css("height", windowH); 
-	    $(".video_container").css("width", windowW); 
+	    $(".video_container").css("width", windowW);
 	    $(".video_header").css("height", videoHeaderH); 
 	}
 
