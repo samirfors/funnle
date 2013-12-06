3a34ba30b50c677f25ca48a7bff404c351df48e6
Christoffer Ekeroth <ekeroth.christoffer@gmail.com>
2013-12-06 22:08:36 +0100

    Remove redundant calls + parentheses

--- app/controllers/followings_controller.rb
+++ app/controllers/followings_controller.rb
@@ -1,10 +1,10 @@
 class FollowingsController < ApplicationController
 
-  def index()
+  def index
     email = params[:email]
     @user = User.where(email: email).first || User.new
-    @users = User.all.to_a
-    @following = @user.followings.to_a
+    @users = User.all
+    @following = @user.followings
   end
 
   def create
